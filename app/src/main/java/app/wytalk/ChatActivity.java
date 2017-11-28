e71bae21384e5b7cb9e6dfd0340f9b9c358c7a9b
closeweb <songje99@naver.com>
2017-11-28 22:34:18 +0900

    v0.32

--- app/src/main/java/app/wytalk/ChatActivity.java
+++ app/src/main/java/app/wytalk/ChatActivity.java
@@ -4,35 +4,91 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class ChatActivity extends AppCompatActivity {
 
+    int check;
     String name;
-    LinearLayout iconLayout;
-    ImageButton button;
+    FrameLayout frameLayout;
+    LinearLayout itemLinear;
+    LinearLayout imoticonLinear;
+
+    ImageButton button1;
+    ImageButton button2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        iconLayout = (LinearLayout)findViewById(R.id.layout);
-        iconLayout.setVisibility(View.GONE);
+        frameLayout = (FrameLayout)findViewById(R.id.layout);
+        itemLinear = (LinearLayout)findViewById(R.id.item_linear);
+        imoticonLinear = (LinearLayout)findViewById(R.id.imoticon_linear);
 
-        button = (ImageButton)findViewById(R.id.imageButton1);
+        frameLayout.setVisibility(View.GONE);
+        check = 0; //basic
+
+
+        button1 = (ImageButton)findViewById(R.id.imageButton1);
+        button2 = (ImageButton)findViewById(R.id.imageButton2);
+
 
         Intent intent = getIntent();
         name =  intent.getStringExtra("User_Name");//"이름"문자 받아옴
         setTitle(name);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                iconLayout.setVisibility(View.VISIBLE);
+                if(check==0){ //basic
+                    check=1;
+                    frameLayout.setVisibility(View.VISIBLE);
+                    itemLinear.bringToFront();
+                }else if(check ==1) {
+                    check =0;
+                    frameLayout.setVisibility(View.GONE);
+                }else if(check==2){
+                    check=1;
+                    itemLinear.bringToFront();
+                }
+
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(check==0){
+                    check=2;
+                    frameLayout.setVisibility(View.VISIBLE);
+                    imoticonLinear.bringToFront();
+                }else if(check==1){
+                    check=2;
+                    imoticonLinear.bringToFront();
+                }else if(check==2){
+                    check =0;
+                    frameLayout.setVisibility(View.GONE);
+                }
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(check==0){ //basic
+            super.onBackPressed();
+        }else{
+            frameLayout.setVisibility(View.GONE);
+            check=0;
+        }
+
     }
 
 }
--- app/src/main/java/app/wytalk/MainActivity.java
+++ app/src/main/java/app/wytalk/MainActivity.java
@@ -1,22 +1,30 @@
 package app.wytalk;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button1;
 
+    EditText idText; //id
+    EditText pwText; //pw
+
     private Socket socket;
-
-    final String ip = "192.168.0.28"; //genymotion host
-    final int port = 9436; // port number
-
+    FirstConnectThread thread;
 
 
     @Override
@@ -24,22 +32,113 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) || (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.INTERNET, Manifest.permission.ACCESS_NETWORK_STATE
+            }, 466);
+        }//인터넷 허가
+
+
+
         button1 = (Button) findViewById(R.id.button1);
+        idText = (EditText)findViewById(R.id.editText1);
+        pwText = (EditText)findViewById(R.id.editText2);
 
 
         button1.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MainViewActivity.class);
-                startActivity(intent);
+
+                System.out.println("----test1");
+                thread = new FirstConnectThread();
+                System.out.println("----test2");
+                thread.start();
+                System.out.println("----test3");
+
+                //Intent intent = new Intent(getApplicationContext(),MainViewActivity.class);
+                //startActivity(intent);
             }
         });
 
 
 
+    }
+
+    class FirstConnectThread extends Thread {
+
+/*      int port = 80;
+        Object input;
+        String output_id;
+        String output_pw;
+        String output_num;
+        SharedPreferences id_pref;
+        SharedPreferences.Editor id_commit;*/
+
+        String output_id;
+        String output_pw;
+        String output; //서버로 보낸 데이터
+        String input; //서버로부터 받은 데이터
+
+        public void run() {
 
 
+            String host = "192.168.0.30";
+            int port = 30015;
 
+//            memberPref = getSharedPreferences("memberPref", MODE_PRIVATE);
+//          final SharedPreferences.Editor memberEditor = memberPref.edit();
+            try {
+                System.out.println("----testa");
+                Socket socket = new Socket(host, port);
+                System.out.println("----testb");
+                System.out.println("서버로 연결되었습니다. : " + host + ", " + port);
+
+                output_id = idText.getText().toString();
+                output_pw = pwText.getText().toString();
+
+                output = "[LOGIN]:"+output_id+"/"+output_pw;
+
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.write(output.getBytes());
+                dataOutputStream.flush();
+
+                System.out.println("send-" + output);
+
+
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                System.out.println("----test4");
+
+                byte [] b = new byte[1024];
+                dataInputStream.read(b);
+                input = new String(b);
+
+                System.out.println("receive-" + input);
+
+
+                System.out.println("----test5");
+                if (input.toString().equals("[LOGIN]:OK")) {
+
+                    System.out.println("test  ok");
+
+                    dataInputStream.close();
+                    dataOutputStream.close();
+                    socket.close();
+
+                    Intent intent = new Intent(getApplicationContext(),MainViewActivity.class);
+                    startActivity(intent);
+
+                }else{
+                    System.out.println("test  nok");
+                    //Toast.makeText(MainActivity.this,"잘못된 정보입니다.",Toast.LENGTH_SHORT).show();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("접근실패");
+                Toast.makeText(MainActivity.this,"접근실패",Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
     }
 
 }
